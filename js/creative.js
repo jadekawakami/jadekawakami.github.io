8fbc3fc757dc4b92161b9705bfa4caad79bc07db
jadir-junior <jadirjsjunior@gmail.com>
2016-10-27 21:19:09 -0200

    producao e styleing starting

--- js/creative.js
+++ js/creative.js
@@ -61,4 +61,12 @@
         }
     });
 
+    $('.grid').masonry({
+      // set itemSelector so .grid-sizer is not used in layout
+      itemSelector: '.grid-item',
+      // use element for option
+      columnWidth: '.grid-sizer',
+      percentPosition: true
+    });
+
 })(jQuery); // End of use strict
