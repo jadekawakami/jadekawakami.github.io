b0556ed1181930fbcd85bb8c9e3288a5c3e71814
jadir-junior <jadirjsjunior@gmail.com>
2016-11-13 21:26:43 -0200

    about

--- gulpfile.js
+++ gulpfile.js
@@ -94,6 +94,6 @@ gulp.task('dev', ['browserSync', 'less', 'minify-css', 'minify-js'], function() 
     gulp.watch('js/*.js', ['minify-js']);
     // Reloads the browser whenever HTML or JS files change
     gulp.watch('*.html', browserSync.reload);
-    gulp.watch('view/*.html', browserSync.reload);
+    gulp.watch('views/*.html', browserSync.reload);
     gulp.watch('js/**/*.js', browserSync.reload);
 });
